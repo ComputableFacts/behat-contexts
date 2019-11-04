ca7c92035e0924301a7a4d15b51755c8f3b1ebc6
Patrick Brisacier <pbrisacier@mncc.fr>
2019-11-04 16:12:54 +0100

    Add the ApiContext based on MinkContext.

---
+++ src/Behat/Context/Laravel/ApiContext.php
@@ -0,0 +1,12 @@
+<?php namespace ComputableFacts\Behat\Context\Laravel;
+
+use Behat\MinkExtension\Context\MinkContext;
+
+/**
+ * @copyright 2019 ComputableFacts
+ * @license Apache 2.0
+ * @author Patrick Brisacier
+ */
+class ApiContext extends MinkContext
+{
+}
